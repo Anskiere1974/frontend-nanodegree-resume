8bfd2cf73a545ee017247b5c8b45832075786d76
Mayer Juergen <mayer.juergen@me.com>
2017-03-02 13:00:03 +0100

    Add Steve Jobs Theme

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,11 +2,11 @@ var bio = {
   "name" : "Steven Paul Jobs",
   "role" : "Genius",
   "contacts" : {
-    "mobile" : "00436769608100",
-    "email" : "mayer.juergen@me.com",
-    "github" : "Anskiere1974",
-    "twitter" : "@Anskiere1974",
-    "location" : "Thoerl"
+    "mobile" : "4089961010",
+    "email" : "steve@mac.com",
+    "github" : "Steve",
+    "twitter" : "@Steve",
+    "location" : "Cupertino, CA 95014"
   },
   "welcomeMessage" : "Innovation distinguishes between a leader and a follower.",
   "skills" : [
@@ -18,27 +18,27 @@ var bio = {
 var education = {
   "schools" : [
     {
-      "name" : "Karl Franzens University Graz",
-      "location" : "Graz, Austria",
-      "degree" : "Master of Science",
-      "majors" : ["Health"],
-      "dates" : "2003",
-      "url" : "https://www.uni-graz.at"
+      "name" : "Reed College",
+      "location" : "3203 SE Woodstock Blvd, Portland, OR 97202, USA",
+      "degree" : "no degree earned",
+      "majors" : ["Caligraphy"],
+      "dates" : "1973",
+      "url" : "http://www.reed.edu/"
     },
     {
-      "name" : "BG BRG Kapfenberg",
-      "location" : "Kapfenberg, Austria",
-      "degree" : "Matura",
+      "name" : "Homestead High School",
+      "location" : "21370 Homestead Rd, Cupertino, CA 95014, USA",
+      "degree" : "High School Diploma",
       "majors" : ["Nature Sciences"],
-      "dates" : "2003",
-      "url" : "http://www.gym-kapfenberg.at/"
+      "dates" : "1969-1972",
+      "url" : "http://www.hhs.fuhsd.org/"
     }
   ],
   "onlineCourses" : [
     {
       "title" : "Nanodegree Front End Web Development",
       "school" : "Udacity",
-      "dates" : "2016",
+      "dates" : "2011",
       "url" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
     }
   ]
@@ -47,18 +47,32 @@ var education = {
 var work = {
   "jobs" : [
     {
-      "employer" : "KAGES Steiermark",
-      "title" : "Dr. med. univ",
-      "location" : "Graz, Austria",
-      "dates" : "2003 - 2009",
-      "description" : "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+      "employer" : "Apple Computer Inc.",
+      "title" : "CEO",
+      "location" : "1 Infinite Loop, Cupertino, CA 95014",
+      "dates" : "1997-2011",
+      "description" : "Helped company to once again create phenomenal products such as IMac, iBook, G4, Power Book, iTools, iMovie, Mac OS X, IPhone, IPod. We create devices that are fun, powerful and easy to use."
     },
     {
-      "employer" : "Gesundheitszentrum",
+      "employer" : "Pixar Animation Studio",
       "title" : "CEO",
-      "location" : "Thoerl, Austria",
-      "dates" : "2009 - in progress",
-      "description" : "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+      "location" : "1200 Park Avenue Emeryville, California 94608",
+      "dates" : "1986-2006",
+      "description" : "Discovered a little animation company that needed a vision. Liked the product so much that I bought the company. Pixar is now the digital animation studio. We have created and produced half of the top grossing animated films of all time, including: Toy story, A Bug's Life, Toy story 2 and Monsters, Inc. Sold the company to Disney in 2006."
+    },
+    {
+      "employer" : "NeXT Software Inc.",
+      "title" : "CEO",
+      "location" : "900 Chesapeake Drive, Redwood City, CA 94063 USA.",
+      "dates" : "1986-1997",
+      "description" : "Founded NeXT Software Inc., which created really cool hardware that exploited the full potential of Object Oriented technologies. NeXT technologies made programmers lives a whole lot easier, thus giving them more leisure time to go catch the latest Pixar release. Sold NeXT to apple in 1997."
+    },
+    {
+      "employer" : "Apple Computer Inc.",
+      "title" : "Co-Founder",
+      "location" : "1 Infinite Loop, Cupertino, CA 95014",
+      "dates" : "1976-1986",
+      "description" : "Invested heavily in funding start-up company (Sold my VW mini-bus). Apple ignited the personal computer revolution with insanely great products such as the Apple II. Took the company public in 1980 at $22 per share. Later in 1984, we reinvented the personal computer with the Macintosh. Left in 1986 to decide which step to take NeXT."
     }
   ]
 };
@@ -66,12 +80,66 @@ var work = {
 var projects = {
   "projects" : [
     {
-      "title" : "Sample Project 1",
-      "dates" : "2017",
-      "description" : "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+      "title" : "Apple Macintosh",
+      "dates" : "1984",
+      "description" : "The 1984 Apple Macintosh computer was a launching point for influential products to come. The design fortified Apple's simplistic style, while uniquely sporting a separate keyboard and mouse. With 1 million units sold, consumers jumped at the chance to use it for business, finance, education, graphic design, and more. When the original 128k model hit stores, consumers were excited about a machine with a 32-bit processor and 512k maximum RAM. Compare that to the 2013 Mac Pro, which offers up to a 3.5 GHz 6-core processor and up to 64 GB of RAM.",
       "images" : [
-        "http://placehold.it/350x150",
-        "http://placehold.it/350x150"
+        "images/mac1.png",
+        "images/mac2.png"
+      ]
+    },
+    {
+      "title" : "Macintosh PowerBook",
+      "dates" : "1991",
+      "description" : "Apple's release of the portable Macintosh PowerBook in 1991 transformed the style and functionality of on-the-go computing. The PowerBook featured a highly compact design for its time, with a built-in rolling trackball, as well as keys situated close to the screen and enough space for the palms to rest comfortably. After an influential 15-year production run, the PowerBook line ended in 2006, superseded by the powerful MacBook Pro.",
+      "images" : [
+        "images/powerbook1.png",
+        "images/powerbook2.png"
+      ]
+    },
+    {
+      "title" : "iMac",
+      "dates" : "1998",
+      "description" : "A colorful step forward, the release of Apple's iMac in 1998 celebrated a union of creativity and communication. The name itself — iMac with a lowercase i — symbolized the importance of the Internet. The semi-clear shell in vibrant Bondi blue allowed consumers to peek into its inner workings and was later available in a total of 13 energetic color options. With plug-and-play simplicity, the iMac took the personal computing market by stylish storm, capturing the hearts of students in particular.",
+      "images" : [
+        "images/imac1.png",
+        "images/imac2.png"
+      ]
+    },
+    {
+      "title" : "iPod",
+      "dates" : "2001",
+      "description" : "In the same year that Apple introduced iTunes, the iPod burst on the scene. The sleek MP3 player brought portable pocket-sized music to shoppers around the globe, simplifying the way people purchased and listened to tunes. With its iconic click-wheel, 10-hour battery life, and space for 1,000 songs, the device had both consumers and competitors scrambling for a listen.",
+      "images" : [
+        "images/ipod1.png",
+        "images/ipod2.png"
+      ]
+    },
+    {
+      "title" : "iPhone",
+      "dates" : "2007",
+      "description" : "The iPhone is undoubtedly one of Apple's most revolutionary devices to date, accounting for more than 50 percent of the company's revenue. After the initial release in 2007, the iPhone has seen several generations that continuously lead in smartphone sale (iPhone 5s 150.2 million sold). Coveted for its intuitive operating system, sleek design, and wide array of apps, the iPhone remains cutting edge.",
+      "images" : [
+        "images/iphone1.png",
+        "images/iphone2.png"
+      ]
+    },
+    {
+      "title" : "iPad",
+      "dates" : "2010",
+      "description" : "Apple made waves in 2010 when it unveiled the iPad, and competitors were left scrambling to get in on the next wave of portable technology. The iPad combined the functionality of an iPod with the versatility of a larger screen and the power of portable computing. The subsequent introduction of the iPad Air in 2013 sent a hefty reverb through the tablet world, with a design almost half the weight of its predecessor and LTE connectivity to boot.",
+      "images" : [
+        "images/ipad1.png",
+        "images/ipad2.png"
+      ]
+    },
+    {
+      "title" : "Apple Watch",
+      "dates" : "2015",
+      "description" : "The Apple Watch, which works in conjunction with the iPhone 5 and later models, redefines what the modern consumer expects from a timepiece. In addition to telling time, it also supports calls, texts, and apps. One of the most notable features is the Digital Touch messaging system, which turns simple finger taps into efficient communication. With three distinct design collections all sporting a sturdy sapphire display, the Apple Watch brings together the company's pioneering technology with sleek style and imaginative function.",
+      "images" : [
+        "images/watch1.png",
+        "images/watch2.png"
       ]
     }
   ]
